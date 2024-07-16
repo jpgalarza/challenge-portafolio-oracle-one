d7fa35fc729fa101e2790e7537718bf54ad10373
jpgalarza <jpgalarza@hotmail.com.ar>
2024-07-16 02:13:52 -0300

    Incorporación de datos personales

--- js/main.js
+++ js/main.js
@@ -222,7 +222,8 @@ const submit = (e) => {
   
   if(validatedForm) {
     const form = document.querySelector('#contacto form[name="form"]');
-    console.log('El formulario se enviara');
+    console.log('El formulario SE ENVÍA');
+    form.submit();
   }else {
     console.log('El formulario NO SE ENVÍA');
   };
